ecd5b52a6adecbc9e285f9c5d18216ca55b62cb1
klporter1219 <78804731+klporter1219@users.noreply.github.com>
2021-08-25 16:43:02 -0400

    all files

---
+++ src/pages/products/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <React.Fragment>
+            <h1>Home</h1>
+        </React.Fragment>
+    );
+};
+
+export default ProductsPage;
