659ef404949fdba2d02b20d27bf0dab98b44a740
klporter1219 <78804731+klporter1219@users.noreply.github.com>
2021-08-25 17:23:48 -0400

    Url update, pathPrefix update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~klsipes/N322/kp-first-app",
     title: "First App",
   },
   plugins: [],
+  pathPrefix: '/~klsipes/N322/kp-first-app'
 };
